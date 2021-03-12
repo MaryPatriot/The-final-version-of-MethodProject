108cbcb4afa11991e59bb6f3cf35d5ff6b9849ff
Mary <mary.vatanparast@gmail.com>
2021-03-12 09:01:38 +0100

    Final Version

--- newerversion of method project 1103/Program.cs
+++ newerversion of method project 1103/Program.cs
@@ -15,10 +15,9 @@ namespace newerversion_of_method_project_1103
             Console.WriteLine("C# 1th project");
             Console.WriteLine("**********************************************************************************************************");
 
-           
-
             string Vehicle;
 
+            //Vehicle input from user, and validation of input data
             do
             {
                 Console.WriteLine("PLease Choose Type of your vehicle: 1:Car, 2:Bus, 3:Truck ");
@@ -27,15 +26,23 @@ namespace newerversion_of_method_project_1103
 
             } while (Vehicle != "Car" && Vehicle != "Bus" && Vehicle != "Truck");
 
-            double[] timearray = new double[4];
-            Inputtimes(timearray);
-            double[] spenttimeArray = new double[3];
+            double[] timearray = new double[4]; //declare an array for user input times: hourenter,minenter, hourleft and minleft
+            Inputtimes(timearray); //Call inputtime method
+            double[] spenttimeArray = new double[3]; // declare an array for calculation of: hourspent, minspent and totalspenttime
 
-            Spenttime(timearray,spenttimeArray); 
+            Spenttime(timearray, spenttimeArray); //Call spenttime method
 
-            double vehicleCharge = VehicleCharge(Vehicle, spenttimeArray[2]);
-         
-            Outputformat(Vehicle, timearray, spenttimeArray[0], spenttimeArray[1], spenttimeArray[2], vehicleCharge);
+            double vehicleCharge = VehicleCharge(Vehicle, spenttimeArray[2]); //call vehiclecharge method 
+
+            if (vehicleCharge == 0)
+            {
+                Console.WriteLine("Error");
+            }
+            else
+            {
+                Outputformat(Vehicle, timearray, spenttimeArray[0], spenttimeArray[1], spenttimeArray[2], vehicleCharge); //Call outpuformat method
+
+            }
 
         }
 
@@ -46,7 +53,7 @@ namespace newerversion_of_method_project_1103
             double Minenter;
             double Hourleft;
             double Minleft;
-         
+
             do
             {
                 Console.WriteLine("Hour vehicle enetered lot: (0-24)");
@@ -64,30 +71,21 @@ namespace newerversion_of_method_project_1103
 
             } while (Hourenter < 0 || Hourenter > 24 || Minenter < 0 || Minenter > 60 || Hourleft < 0 || Hourleft > 24 || Minleft < 0 || Minleft > 60);
 
-
-            foreach (double item in timearray)
-            {
-                Console.WriteLine(item);
- 
-            }
-
         }
 
-        // Method to calculate actual time spent in parking by vehicle. 
-        static void Spenttime(double[] timearray, double [] spenttimeArray) 
+        // Method to calculate actual time spent in parking by vehicle. Using modulo arithmetic to handle time calculation.
+        static void Spenttime(double[] timearray, double[] spenttimeArray)
         {
             double hourspent = 0;
             double minspent = 0;
-            
 
             if (timearray[3] < timearray[1])
             {
-           
-                hourspent = (timearray[2]-1) - timearray[0];
+
+                hourspent = (timearray[2] - 1) - timearray[0];
                 spenttimeArray[0] = Convert.ToInt32(hourspent);
 
-
-                minspent = (timearray[3]+60) - timearray[1];
+                minspent = (timearray[3] + 60) - timearray[1];
                 spenttimeArray[1] = Convert.ToInt32(minspent);
 
                 double Totalspenttime = hourspent + Math.Ceiling((minspent / 15) * 0.25);
@@ -103,16 +101,11 @@ namespace newerversion_of_method_project_1103
                 minspent = timearray[3] - timearray[1];
                 spenttimeArray[1] = Convert.ToInt32(minspent);
 
-
                 double Totalspenttime = hourspent + Math.Ceiling((minspent / 15) * 0.25);
                 spenttimeArray[2] = Convert.ToInt32(Totalspenttime);
-          
-            }
-            foreach (double item in spenttimeArray)
-            {
-                Console.WriteLine(item);
 
             }
+
         }
 
         // Method to calculate parking charge!
@@ -123,7 +116,7 @@ namespace newerversion_of_method_project_1103
             if (vehicle == "Car" && Totalspenttime <= 3)
             {
                 double VehicleCharge = 0;
-                
+
                 return VehicleCharge;
 
             }
@@ -131,27 +124,26 @@ namespace newerversion_of_method_project_1103
             {
                 double Newtime = Totalspenttime - 3;
                 double VehicleCharge = Newtime * 1.50;
-              
+
                 return VehicleCharge;
             }
-
             else if (vehicle == "Bus" && Totalspenttime <= 1)
             {
                 double VehicleCharge = Totalspenttime * 2.00;
-                
+
                 return VehicleCharge;
             }
             else if (vehicle == "Bus" && Totalspenttime > 1)
             {
                 double Newtime = Totalspenttime - 1;
                 double VehicleCharge = Newtime * 3.70 + Totalspenttime * 2.00;
-               
+
                 return VehicleCharge;
             }
             else if (vehicle == "Truck" && Totalspenttime <= 2)
             {
                 double VehicleCharge = Totalspenttime * 1.00;
-              
+
                 return VehicleCharge;
 
             }
@@ -159,15 +151,14 @@ namespace newerversion_of_method_project_1103
             {
                 double Newtime = Totalspenttime - 2;
                 double VehicleCharge = Newtime * 2.50 + Totalspenttime * 1.00;
-                
+
                 return VehicleCharge;
             }
             else
             {
-              
+
                 return 0;
             }
-
 
         }
         //Method for output format!
@@ -180,8 +171,6 @@ namespace newerversion_of_method_project_1103
             Console.WriteLine($"ROUNDED TOTAL: {totalspenttime}");
             Console.WriteLine($"TOTAL CHARGES: {vehicleCharge} $");
 
-
         }
     }
-    }
-
+}
